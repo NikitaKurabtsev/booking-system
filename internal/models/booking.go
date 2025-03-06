32cfad600a70f95f0f6b2ea7148307162910f35e
nikitaKurabtsev <nick3500hunna@gmail.com>
2025-03-06 15:27:42 +0300

    refactor: rename db tables

--- internal/models/booking.go
+++ internal/models/booking.go
@@ -1,31 +1,32 @@
 package models
 
 import (
+	"errors"
 	"time"
 )
 
 type Booking struct {
-	ID         string    `json:"id,omitempty"`
-	ResourceID string    `json:"resource-id,omitempty"`
-	UserID     int       `json:"user-id,omitempty"`
-	StartTime  time.Time `json:"start-time"`
-	EndTime    time.Time `json:"end-time"`
+	ID         string    `json:"id,omitempty" db:"id"`
+	UserID     int       `json:"user_id,omitempty" db:"user_id"`
+	ResourceID string    `json:"resource_id,omitempty" db:"resource_id"`
+	StartTime  time.Time `json:"start_time" db:"start_time"`
+	EndTime    time.Time `json:"end_time" db:"end_time"`
 }
 
-// type UpdateBookingInput struct {
-// 	StartTime *time.Time `json:"start-time"`
-// 	EndTime   *time.Time `json:"end-time"`
-// }
+type UpdateBookingInput struct {
+	StartTime *time.Time `json:"start_time"`
+	EndTime   *time.Time `json:"end_time"`
+}
 
-// func (r *UpdateBookingInput) Validate() error {
-// 	if r.StartTime == nil && r.EndTime == nil {
-// 		return errors.New("at least one of start or end time must be provided")
-// 	}
-// 	if r.StartTime != nil && r.EndTime != nil {
-// 		if r.StartTime.After(*r.EndTime) {
-// 			return errors.New("start time cannot be latter than end time")
-// 		}
-// 	}
+func (r *UpdateBookingInput) Validate() error {
+	if r.StartTime == nil && r.EndTime == nil {
+		return errors.New("at least one of start or end time must be provided")
+	}
+	if r.StartTime != nil && r.EndTime != nil {
+		if r.StartTime.After(*r.EndTime) {
+			return errors.New("start time cannot be latter than end time")
+		}
+	}
 
-// 	return nil
-// }
+	return nil
+}
