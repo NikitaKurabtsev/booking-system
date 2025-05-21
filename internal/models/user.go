301a526532fcda3e56c99897549ea27a265dccdb
nikitaKurabtsev <nick3500hunna@gmail.com>
2025-05-21 12:03:55 +0300

    chore: remove models dir

--- internal/models/user.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type User struct {
-	ID           int    `db:"id" json:"id"`
-	Username     string `db:"username" json:"username,omitempty"`
-	Email        string `db:"email" json:"email,omitempty"`
-	PasswordHash string `db:"password_hash"`
-}
