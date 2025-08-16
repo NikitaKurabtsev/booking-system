83bda5df5c6733d53b3bd3ec19fa2ee145636f80
nikitaKurabtsev <nick3500hunna@gmail.com>
2025-08-16 06:55:46 +0300

    added handlers tests

--- internal/handlers/booking_test.go
+++ internal/handlers/booking_test.go
@@ -1,1 +1,159 @@
 package handlers
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"github.com/NikitaKurabtsev/booking-system/internal/handlers/dto"
+	"github.com/NikitaKurabtsev/booking-system/internal/services"
+	mocksbookingservice "github.com/NikitaKurabtsev/booking-system/internal/services/mocks"
+	mocklogger "github.com/NikitaKurabtsev/booking-system/logger/mocks"
+	"github.com/gin-gonic/gin"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHandler_createBooking(t *testing.T) {
+	type mockBehavior func(s *mocksbookingservice.MockBooking, logger *mocklogger.MockLogger)
+
+	tests := []struct {
+		name                 string
+		mockBehavior         mockBehavior
+		inputData            dto.CreateBookingDTO
+		requestMethod        string
+		expectedCodeStatus   int
+		expectedResponseBody string
+	}{
+		{
+			name: "Ok",
+			mockBehavior: func(s *mocksbookingservice.MockBooking, l *mocklogger.MockLogger) {
+				s.EXPECT().
+					CreateBooking(gomock.Any(), gomock.Any()).
+					Return(1, nil)
+			},
+			inputData: dto.CreateBookingDTO{
+				ResourceID: 1,
+				UserID:     0,
+				StartTime:  "2025-11-01 13:00:00",
+				EndTime:    "2025-11-01 14:00:00",
+			},
+			requestMethod:        "POST",
+			expectedCodeStatus:   201,
+			expectedResponseBody: `{"booking_id":1"}`,
+		},
+		{
+			name: "Bad Request",
+			mockBehavior: func(s *mocksbookingservice.MockBooking, l *mocklogger.MockLogger) {
+				s.EXPECT().
+					CreateBooking(gomock.Any(), gomock.Any()).
+					Return(0, errors.New("something went wrong"))
+				l.EXPECT().Error("error", "something went wrong")
+			},
+			inputData: dto.CreateBookingDTO{
+				ResourceID: 1,
+				UserID:     0,
+				StartTime:  "2023-11-01 13:00:00",
+				EndTime:    "2025-11-01 14:00:00",
+			},
+			requestMethod:        "POST",
+			expectedCodeStatus:   400,
+			expectedResponseBody: `{"error":""something went wrong"}`},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+
+			mockLogger := mocklogger.NewMockLogger(ctrl)
+			mockBookingService := mocksbookingservice.NewMockBooking(ctrl)
+
+			test.mockBehavior(mockBookingService, mockLogger)
+
+			bookingService := &services.Service{Booking: mockBookingService}
+			handler := &Handler{service: bookingService, logger: mockLogger}
+
+			w := httptest.NewRecorder()
+
+			jsonData, _ := json.Marshal(test.inputData)
+			req := httptest.NewRequest(test.requestMethod, "/booking", bytes.NewReader(jsonData))
+
+			router := gin.New()
+			router.Use(func(c *gin.Context) {
+				c.Set("userID", 1)
+				c.Next()
+			})
+			router.Handle(test.requestMethod, "/booking", handler.createBooking)
+
+			router.ServeHTTP(w, req)
+
+			assert.Equal(t, test.expectedCodeStatus, w.Code)
+
+		})
+	}
+}
+
+func TestHandler_deleteBooking(t *testing.T) {
+	type mockBehavior func(s *mocksbookingservice.MockBooking, logger *mocklogger.MockLogger)
+
+	tests := []struct {
+		name                 string
+		mockBehavior         mockBehavior
+		requestMethod        string
+		expectedCodeStatus   int
+		expectedResponseBody string
+	}{
+		{
+			name: "Ok",
+			mockBehavior: func(s *mocksbookingservice.MockBooking, l *mocklogger.MockLogger) {
+				s.EXPECT().
+					DeleteBooking(gomock.Any(), 1).
+					Return(nil)
+
+			},
+			requestMethod:        "DELETE",
+			expectedCodeStatus:   204,
+			expectedResponseBody: ``,
+		},
+		{
+			name: "Bad Request",
+			mockBehavior: func(s *mocksbookingservice.MockBooking, l *mocklogger.MockLogger) {
+				s.EXPECT().
+					DeleteBooking(gomock.Any(), 1).
+					Return(errors.New("something went wrong"))
+				l.EXPECT().
+					Error("error", "something went wrong")
+			},
+			requestMethod:        "DELETE",
+			expectedCodeStatus:   400,
+			expectedResponseBody: `{"error":"something went wrong"}`,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+
+			mockLogger := mocklogger.NewMockLogger(ctrl)
+			mockBookingService := mocksbookingservice.NewMockBooking(ctrl)
+
+			test.mockBehavior(mockBookingService, mockLogger)
+
+			bookingService := &services.Service{Booking: mockBookingService}
+			handler := &Handler{service: bookingService, logger: mockLogger}
+
+			w := httptest.NewRecorder()
+			req := httptest.NewRequest(test.requestMethod, "/bookings/1", nil)
+
+			router := gin.New()
+			router.Handle(test.requestMethod, "/bookings/:id", handler.deleteBooking)
+
+			router.ServeHTTP(w, req)
+
+			assert.Equal(t, test.expectedCodeStatus, w.Code)
+			assert.Equal(t, test.expectedResponseBody, w.Body.String())
+
+		})
+	}
+}
--- internal/handlers/resource_test.go
+++ internal/handlers/resource_test.go
@@ -1,1 +1,77 @@
 package handlers
+
+import (
+	"errors"
+	"github.com/NikitaKurabtsev/booking-system/internal/domain"
+	"github.com/NikitaKurabtsev/booking-system/internal/services"
+	resourceservicemock "github.com/NikitaKurabtsev/booking-system/internal/services/mocks"
+	loggermock "github.com/NikitaKurabtsev/booking-system/logger/mocks"
+	"github.com/gin-gonic/gin"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHandler_getAllResources(t *testing.T) {
+	type mockBehavior func(s *resourceservicemock.MockResource, l *loggermock.MockLogger)
+
+	mockData := []domain.Resource{
+		{ID: 1, Name: "office", Type: "available"},
+	}
+
+	tests := []struct {
+		name                 string
+		mockBehavior         mockBehavior
+		expectedCodeStatus   int
+		expectedResponseBody string
+	}{
+		{
+			name: "Ok",
+			mockBehavior: func(s *resourceservicemock.MockResource, l *loggermock.MockLogger) {
+				s.EXPECT().GetResources(gomock.Any()).Return(mockData, nil)
+			},
+			expectedCodeStatus:   200,
+			expectedResponseBody: `{"data":[{"id":1,"name":"office","type":"available"}]}`,
+		},
+		{
+			name: "Service Error",
+			mockBehavior: func(s *resourceservicemock.MockResource, l *loggermock.MockLogger) {
+				s.EXPECT().GetResources(gomock.Any()).Return(nil, errors.New("something went wrong"))
+				l.EXPECT().Error("error", "something went wrong")
+			},
+			expectedCodeStatus:   500,
+			expectedResponseBody: `{"error":"something went wrong"}`,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+
+			mockLogger := loggermock.NewMockLogger(ctrl)
+			mockResourceService := resourceservicemock.NewMockResource(ctrl)
+
+			test.mockBehavior(mockResourceService, mockLogger)
+
+			service := &services.Service{Resource: mockResourceService}
+
+			handler := &Handler{
+				logger:  mockLogger,
+				service: service,
+			}
+
+			w := httptest.NewRecorder()
+			req := httptest.NewRequest(http.MethodGet, "/resources", nil)
+
+			router := gin.New()
+			router.GET("/resources", handler.getAllResources)
+
+			router.ServeHTTP(w, req)
+
+			assert.Equal(t, test.expectedResponseBody, w.Body.String())
+			assert.Equal(t, test.expectedCodeStatus, w.Code)
+		})
+	}
+}
